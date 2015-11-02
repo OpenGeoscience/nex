5933ec0385698c79e8e645b187af4b62f7f68097
Christopher Kotfila <chris.kotfila@kitware.com>
2015-11-02 11:39:56 -0500

    Adding local and aws deployment playbooks

---
+++ deploy/aws/all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SESSION_DEFAULT=NEX
+SESSION=${1-SESSION_DEFAULT}
+
+CMD_DEFAULT=""
+CMD=${2-CMD_DEFAULT}
+
+KEYPATH=/home/kotfic/kitware/projects/NEX/keys/kotfic-NEX.pem
+i=0
+
+tmux kill-session -t NEX
+tmux new-session -d -s NEX
+
+for ip in $(aws --profile NEX \
+                ec2 describe-instances  \
+                --query Reservations[*].Instances[*].PublicIpAddress[] \
+                   | grep "^    \"" \
+                   | sed -e "s/\"//g" \
+                   | sed -e "s/,//g"); do
+
+    if [ "$i" -ne "0" ]; then
+        tmux new-window -tNEX:$i
+    fi
+
+    tmux send-keys -t NEX:$i 'ssh -o StrictHostKeyChecking=no -i '$KEYPATH' ubuntu@'$ip  C-m
+    tmux rename-window $ip
+    tmux send-keys -t NEX:$i $CMD  C-m
+
+    ((i+=1))
+
+done
+
+tmux attach -t NEX
---
+++ deploy/aws/headnode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###
+#  Log in to headnode by querying AWS for the IP address
+
+
+IP_ADDR=$(aws --profile NEX ec2 \
+              describe-instances \
+              --filter "Name=tag:ec2_pod_instance_name,Values=head" \
+              --query Reservations[0].Instances[0].PublicIpAddress \
+              --output text)
+
+ssh -i ../keys/kotfic-NEX.pem ubuntu@$IP_ADDR $*
