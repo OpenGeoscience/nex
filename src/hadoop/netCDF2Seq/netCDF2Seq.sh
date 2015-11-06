849fcf76046b7d0f7a6a976da949f81840a688c3
Christopher Kotfila <chris.kotfila@kitware.com>
2015-11-06 09:39:18 -0500

    Updating netCDF2Seq to be more general

--- src/hadoop/netCDF2Seq/netCDF2Seq.sh
+++ src/hadoop/netCDF2Seq/netCDF2Seq.sh
@@ -4,11 +4,11 @@ BASE_DIR=${1:-/data/tmp}
 HDFS_ROOT=${cat /opt/hadoop/2.7.1/etc/hadoop/core-site.xml | grep "hdfs://" | cut -f2 -d">" | cut -f1 -d"<"}
 
 for NC_NAME in $(ls $BASE_DIR); do
-	BASE_NAME=${NC_NAME%.nc}
+        BASE_NAME=${NC_NAME%.nc}
 
-	time java -jar /home/ubuntu/netCDF2Seq-1.0-SNAPSHOT.jar \
-	          file://$BASE_DIR/$NC_NAME \
-	          $HDFS_ROOT/user/nex/$BASE_NAME.seq
+        time java -jar /home/ubuntu/netCDF2Seq.jar \
+             file://$BASE_DIR/$NC_NAME \
+             $HDFS_ROOT/user/nex/$BASE_NAME.seq
 
-	echo "Finished with /user/nex/$BASE_NAME.seq"
+        echo "Finished with /user/nex/$BASE_NAME.seq"
 done
