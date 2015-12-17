ea51b86318018eff32b63b4e2fd8f86b9f894bcc
Christopher Kotfila <chris.kotfila@kitware.com>
2015-12-17 15:11:45 -0500

    Basic bash script to DL all GDDP files in parallel

--- deploy/aws/all.sh
+++ deploy/aws/all.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
-SESSION_DEFAULT=NEX
-SESSION=${1-SESSION_DEFAULT}
-
-CMD_DEFAULT=""
-CMD=${2-CMD_DEFAULT}
-
-KEYPATH=../keys/aws-NEX.pem
+SESSION=NEX
 i=0
 
-tmux kill-session -t NEX
-tmux new-session -d -s NEX
+run_command ()
+{
+    tmux list-windows -t $SESSION|cut -d: -f1|xargs -I{} tmux send-keys -t $SESSION:{} "$*" C-m
+}
 
-for ip in $(aws --profile NEX \
-                ec2 describe-instances  \
-                --query Reservations[*].Instances[*].PublicIpAddress[] \
-                   | grep "^    \"" \
-                   | sed -e "s/\"//g" \
-                   | sed -e "s/,//g"); do
+tmux new-session -d -s $SESSION 2>/dev/null
+if [ $? -eq 0 ]; then
 
-    if [ "$i" -ne "0" ]; then
-        tmux new-window -tNEX:$i
+    for ip in $(head -n2 ip.list); do
+        if [ "$i" -ne "0" ]; then
+            tmux new-window -t$SESSION:$i
+        fi
+        tmux send-keys -t $SESSION:$i 'ssh -o StrictHostKeyChecking=no -o ControlPersist=4h ubuntu@'$ip  C-m
+        tmux send-keys -t $SESSION:$i "tmux new-session -s import || tmux attach -t import" C-m
+        ((i+=1))
+    done
+    tmux attach -t NEX
+else
+    if [ "$#" -gt 0 ]; then
+        run_command $*
     fi
-
-    tmux send-keys -t NEX:$i 'ssh -o StrictHostKeyChecking=no -i '$KEYPATH' ubuntu@'$ip  C-m
-    tmux rename-window $ip
-    tmux send-keys -t NEX:$i $CMD  C-m
-
-    ((i+=1))
-
-done
-
-tmux attach -t NEX
+fi
