37cf149148c85bb9d3548abdf40fb5181b356226
Chris Kotfila <chris.kotfila@kitware.com>
2016-01-17 19:45:24 +0000

    pull celery master hostname from file

--- experimental/celery_performance/perftest/__init__.py
+++ experimental/celery_performance/perftest/__init__.py
@@ -1,8 +1,12 @@
 from __future__ import absolute_import
 from celery import Celery, Task
+import os
+
+with open(os.path.dirname(__file__) + "/.master_hostname", "r") as fh:
+    master_hostname = fh.read().rstrip()
 
 
 app = app = Celery('perftests',
                    backend='amqp',
-                   broker='amqp://guest:guest@ec2-52-11-87-22.us-west-2.compute.amazonaws.com',
+                   broker='amqp://guest:guest@{}'.format(master_hostname),
                    include=['perftest.tests'])
