706b378cca4f27a1f87dd116cee43dea54520cf8
doruk.ozturk <doruk.ozturk@kitware.com>
2016-06-03 10:38:42 -0400

    Download methods are added

--- utility/download.py
+++ utility/download.py
@@ -8,13 +8,13 @@ from boto3.s3.transfer import S3Transfer
 class Downloader(object):
 
 
-    def __init__(self, year, day, month):
+    def __init__(self, year, month, day):
         self.year = year
         self.month = month
         self.day = day
 
         self.directory = self._create_data_directory()
-        self.bucket = self._connect_to_bucket()
+        self.nasanex = self._connect_to_bucket()
 
 
     def _create_data_directory(self):
@@ -36,7 +36,6 @@ class Downloader(object):
         client = resource.meta.client
         transfer = S3Transfer(client)
 
-
         # Connect anonymously
         resource.meta.client.meta.events.register('choose-signer.s3.*',
                                                   disable_signing)
@@ -44,19 +43,38 @@ class Downloader(object):
         # Get the nasanex bucket
         nasanex = resource.Bucket('nasanex')
 
-        return nasanex
+        return {'bucket': nasanex, 'transfer': transfer}
+
+
+    def _filter_and_download_data(self, prefix):
+
+        for item in self.nasanex['bucket'].objects.filter(Prefix=prefix):
+            output_dir = os.path.join(self.directory, os.path.basename(item.key))
+            self.nasanex['transfer'].download_file('nasanex', item.key, output_dir)
+
 
     def download_aqua_data(self):
-        pass
+
+        prefix = '{}/{}.{}.{}'.format('MODIS/MOLA/MYD13Q1.005',
+            self.year, self.month, self.day)
+
+        self._filter_and_download_data(prefix)
 
     def download_terra_data(self):
-        pass
 
-    def download_weather_data(self):
-        pass
+        prefix = '{}/{}.{}.{}'.format('MODIS/MOLT/MOD13Q1.005',
+            self.year, self.month, self.day)
 
+        self._filter_and_download_data(prefix)
+
+    def download_tmax_data(self):
+
+        prefix = 'NEX-GDDP/BCSD/rcp45/day/atmos/tasmax/r1i1p1/v1.0/\
+tasmax_day_BCSD_rcp45_r1i1p1_CCSM4_{}.nc'.format(self.year)
+
+        self._filter_and_download_data(prefix)
 
 if __name__ == '__main__':
     d = Downloader("2013", "08", "05")
     d.download_aqua_data()
-    print d
+    d.download_tmax_data()
