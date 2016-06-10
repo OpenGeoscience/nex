ceda93d427be686029869ced1d20ba389da86c13
Christopher Kotfila <chris.kotfila@kitware.com>
2016-06-10 10:30:42 -0400

    Fix up App.scala to return winkler scale

--- src/scala/days_over_freezing/src/main/scala/com/kitware/App.scala
+++ src/scala/days_over_freezing/src/main/scala/com/kitware/App.scala
@@ -14,6 +14,7 @@ object App {
     val sc = new org.apache.spark.SparkContext(conf)
 
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    import sqlContext.implicits._
 
     val parquet = sqlContext.read.parquet(args(0))
     parquet.registerTempTable("parquet")
@@ -30,13 +31,48 @@ object App {
     def _over_freezing(k: Double) = if (k > 273.15) 1 else 0
     val over_freezing = udf(_over_freezing(_: Double))
 
-    val of = df.withColumn("o_f", over_freezing(col("tasmin")))
+    val of = df.withColumn("o_f", over_freezing($"tasmin"))
 
     val spec = Window.partitionBy("lat", "lon").orderBy("time").rowsBetween(-200, 0)
 
-    val over = of.select(col("*"), sum(col("o_f")).over(spec) as "days_over")
+    val over = of.select($"*", sum($"o_f").over(spec) as "days_over")
 
-    over.select("*").where(col("days_over") >= 200).write.format("csv").save(args(1))
+    val wine_locs = over.select($"lat", $"lon").where($"days_over" >= 200).distinct()
+
+    val wine = over.join(wine_locs, Seq("lat", "lon"))
+
+    wine.write.format("parquet").save(args(1))
+
+    // User defined function for average temperature in fahrenheit
+    def _k_to_f(k: Double): Double = ((k - 273.15) * 1.8) + 32.0
+    val avg_tmp_f = udf((min: Double, max: Double) => (_k_to_f(min) + _k_to_f(max)) / 2.0)
+
+    // See: https://en.wikipedia.org/wiki/Winkler_scale
+    // Months for northern/southern hemisphere
+    val grow_season = parquet.select($"*", avg_tmp_f($"tasmin", $"tasmax") as "f_tasavg").filter(
+      "(lat >= 0.0 AND month >= 4 AND month <= 10) OR (lat < 0.0 AND month <= 4 AND month >= 10)")
+
+
+    // UDF for degree_days
+    val degree_days = udf((temp: Double) => if( (temp.toInt - 50) <= 0 ) 0 else temp.toInt - 50)
+
+
+    val anual_degree_days = grow_season.select($"*", degree_days($"f_tasavg") as "degree_days").groupBy($"year", $"lat", $"lon").agg("degree_days" -> "sum").withColumnRenamed("sum(degree_days)", "degree_days")
+
+
+    def _winkler_scale(d: Double): Int = {
+      if( d <= 2500.0 ) 1
+      else if( d >= 2501.0 && d <= 3000.0) 2
+      else if( d >= 3001.0 && d <= 3500.0) 3
+      else if( d >= 3501.0 && d <= 4000.0) 4
+      else 5
+    }
+
+    val winkler_scale = udf(_winkler_scale(_: Double))
+
+    val winkler = anual_degree_days.withColumn("winkler", winkler_scale($"degree_days"))
+
+    winkler.write.format("csv").save(args(2))
 
   }
 
