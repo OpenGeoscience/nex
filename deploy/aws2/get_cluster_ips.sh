2c8c932689d35418b351447061350f44bc5fd7a8
Christopher Kotfila <chris.kotfila@kitware.com>
2016-07-27 11:42:57 -0400

    Change workflow to include ami generation
    Previously:
    Launch -> Provision -> Do Stuff -> Terminate
    
    Now:
    Launch -> Provision -> Generate AMI -> Terminate
    Launch From AMI -> Update Configs/Format HDFS -> Do Stuff -> Terminate

--- deploy/aws2/get_cluster_ips.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-aws --profile=NEX ec2 describe-instances \
-    --filter="Name=tag:ec2_pod_instance_name,Values=data,persist" \
-    --query Reservations[*].Instances[*].PrivateIpAddress | \
-    # Remove json formating
-    sed -e 's/\[//g' | sed -e 's/\]//g' | sed -e 's/\"//g' | sed -e 's/,//g' | \
-    # Remove space and newlines
-    tr -d " " | grep -v "^$" > ip.list
